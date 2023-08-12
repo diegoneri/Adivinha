e28f9a08331ecf548034311c0993077c35980d8e
Diego Neri <diego@diegoneri.com>
2023-08-11 22:30:17 -0300

    Finaliza programa Adivinha.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("O que é, o que é...");
+Console.WriteLine("... que o nadador faz para bater o recorde?");
+
+Thread.Sleep(5000);
+
+Console.WriteLine();
+Console.WriteLine("Nada.");
